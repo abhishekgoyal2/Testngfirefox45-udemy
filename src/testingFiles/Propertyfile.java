e73f5833b99712a92d4792de6ade428314057549
abhishekgoyal2 <abhi25goel@gmail.com>
2018-04-18 00:08:54 +0530

    test

--- src/testingFiles/Propertyfile.java
+++ src/testingFiles/Propertyfile.java
@@ -1,9 +1,11 @@
 package testingFiles;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,29 +15,49 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Propertyfile {
-	public WebDriver driver;
-	@Test
-	public void Login() throws IOException
-	{
-		Properties prop =new Properties();
-		FileInputStream fis=new FileInputStream("C:\\Users\\Abhishek\\eclipse-workspace\\TestNGTutorial-1\\TestNGTutorial\\src\\testingFiles\\datadriven.properties"); 
+	public WebDriver driver =null;
+	File file =new File("C:\\Users\\Abhishek\\eclipse-workspace\\TestNGTutorial-1\\TestNGTutorial\\src\\testingFiles\\datadriven.properties");
+	Properties prop =new Properties();
 	
-	prop.load(fis);
-	if(prop.getProperty("browser").equals("firefox"))
+
+	@Test
+	public void Login() throws InterruptedException 
+	{
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+
+		try {
+			prop.load(fis);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+	if(prop.getProperty("browser").equalsIgnoreCase("firefox"))
 	{
 		 driver=new FirefoxDriver();
+		 System.out.println("pass");
 	}
-	if(prop.getProperty("browser").equals("chrome"))
-	{
-		 driver=new ChromeDriver();
-	}
-	else
-	{
-		 driver=new InternetExplorerDriver();
-	}
+//	if(prop.getProperty("browser")=="chrome")
+//	{
+//		 driver=new ChromeDriver();
+//	}
+//	else
+//	{
+//		 driver=new InternetExplorerDriver();
+//	}
 	
-	driver.get(prop.getProperty("url	"));
-	System.out.println(prop.getProperty("username"));
+//	driver.get(prop.getProperty("url"));
+//	Thread.sleep(500);
+//	System.out.println(prop.getProperty("url"));
+//	System.out.println(prop.getProperty("browser"));
+
 	}
 	
 }
